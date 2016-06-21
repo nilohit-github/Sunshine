f092501f2c532deec0accf29b89a84ae2e7a3d7a
nilohit-github <jhanilohit9286@gmail.com>
2016-06-20 21:14:25 -0500

    Implementing GCM
    issue with sending push notification

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -16,12 +16,18 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.android.sunshine.app.gcm.RegistrationIntentService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 
@@ -29,6 +35,8 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
 
     private boolean mTwoPane;
     private String mLocation;
@@ -61,7 +69,20 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
         SunshineSyncAdapter.initializeSyncAdapter(this);
+
+        if (checkPlayServices()) {
+                        // Because this is the initial creation of the app, we'll want to be certain we have
+                        // a token. If we do not, then we will start the IntentService that will register this
+                        // application with GCM.
+                        SharedPreferences sharedPreferences =
+                                PreferenceManager.getDefaultSharedPreferences(this);
+                        boolean sentToken = sharedPreferences.getBoolean(SENT_TOKEN_TO_SERVER, false);
+                        if (!sentToken) {
+                            Intent intent = new Intent(this, RegistrationIntentService.class);
+                            startService(intent);
+                       }
         }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -151,4 +172,24 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             startActivity(intent);
         }
     }
+    /**
+     +     * Check the device to make sure it has the Google Play Services APK. If
+     +     * it doesn't, display a dialog that allows users to download the APK from
+     +     * the Google Play Store or enable it in the device's system settings.
+     +     */
+        private boolean checkPlayServices() {
+            GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+            int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.SUCCESS) {
+                if (apiAvailability.isUserResolvableError(resultCode)) {
+                    apiAvailability.getErrorDialog(this, resultCode,
+                            PLAY_SERVICES_RESOLUTION_REQUEST).show();
+             } else {
+                   Log.i(LOG_TAG, "This device is not supported.");
+                  finish();
+                }
+                return false;
+           }
+           return true;
+        }
 }
